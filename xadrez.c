6d802a5d330fe06ea546bb126c5413f88bbbbc53
Julia Falp <julia.falquete@gmail.com>
2025-04-13 08:57:05 -0300

    Desafio Mestre

--- xadrez.c
+++ xadrez.c
@@ -1,44 +1,75 @@
 #include <stdio.h>
 
-int i = 0, e = 0, j = 0, c = 0, d = 0;
+
+void moverTorre(int casas)
+{
+    //Valida a quantidade de casas até zerar
+    if (casas > 0)
+    {
+        printf("Direita\n");
+        moverTorre(casas - 1);// chamando o próprio método recursivamente diminuindo em 1 a quantidade de casas a ser mostradas
+    }
+}
+
+void moverRainha(int casas)
+{
+    //Valida a quantidade de casas até zerar
+    if (casas > 0)
+    {
+        printf("Esquerda\n");
+        moverRainha(casas - 1); // chamando o próprio método recursivamente diminuindo em 1 a quantidade de casas a ser mostradas
+    }
+}
+
+void moverBispo(int casasPercorrer, int caminhoAtual)
+{
+    //Valida se a quantidade de vezes executadas é igual a quantidade de casas solicitadas
+    if (casasPercorrer == caminhoAtual)
+    {
+        printf("Caminho encerrado \n");
+        return;
+    }
+
+    // Condição dentro do for executa apenas uma vez
+    for (int i = -1; i < 1; i += 2)
+    {
+        printf("Direita / ");
+
+        // Condição dentro do for executa apenas uma vez
+        for (int j = -1; j < 1; j += 2)
+        {
+            printf("Cima \n");
+
+            //Chama a função novamente incrementando o parâmetro caminhoAtual para que seja imprimido a quantidade correta de casas solicitadas
+            moverBispo(casasPercorrer, caminhoAtual + 1);
+        }
+    }
+}
+
+void moverCavalo(int casas)
+{
+    //valida as casas a ser andadas em um sentido e depois no outro
+    while (casas > 0)
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Cima \n");
+        }
+
+        printf("Direita \n");
+        break;// break para não ficar no loop infinito
+    }
+}
 
 int main()
 {
-    // Mover a Torre 5 casas para a direita
-    printf("Torre:\n");
-    for (int i = 0; i < 5; i++)
-    {
-        printf("Direita\n");
-    }
-
-    // Mover o Bispo 5 casas para cima e direita
-    while (j < 5)
-    {
-        printf("Cima/Direita\n");
-        j++;
-    }
-
-    // Mover a Rainha 8 casas para a esquerda
-    printf("\nRainha:\n");
-    do
-    {
-        printf("Esquerda\n");
-        e++;
-    } while (e < 8);
-
-    //Mover o Cavalo 2 casas para baixo e 1 esquerda
-    printf("\nCavalo:\n");
-    for (int c = 0; c < 1; c++)
-    {
-        while (d < 2)
-        {
-            printf("Baixo\n");
-            d++;
-        }
-        
-        printf("Esquerda\n");
-    }
-    
-
+    printf("**********Torre************\n");
+    moverTorre(5);
+    printf("**********Rainha***********\n");
+    moverRainha(8);
+    printf("**********Bispo************\n");
+    moverBispo(5, 0);
+    printf("***********Cavalo***********\n");
+    moverCavalo(2);
     return 0;
 }
